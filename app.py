7cb050c86b3f12f6a22487f3d76d5b2624d6e432
carolayramonp-jpg <carolayramonp@gmail.com>
2026-06-13 23:15:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("Proyecto Final Diploma BI")
 
 st.sidebar.title("Parámetros")
+
+st.image("Python_logo.png",width = 500)
+st.sidebar.image("dmc_logo.png",width = 100)
